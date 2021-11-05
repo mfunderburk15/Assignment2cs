bb9c1fe255fca97dd95ae9099c65badad04e37ae
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 15:39:07 -0500

    Question 1 answered

--- Assignment2cs/Program.cs
+++ Assignment2cs/Program.cs
@@ -6,7 +6,16 @@ namespace Assignment2cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.Display the count on the console.
+            int count = 0;
+            for (int i = 1; i <=100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
         }
     }
 }
