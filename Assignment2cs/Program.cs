1e630d3e17f30894e6611c4cc623ddc531c2ed43
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 16:07:40 -0500

    finished problem 2

--- Assignment2cs/Program.cs
+++ Assignment2cs/Program.cs
@@ -7,15 +7,37 @@ namespace Assignment2cs
         static void Main(string[] args)
         {
             //Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.Display the count on the console.
-            int count = 0;
-            for (int i = 1; i <=100; i++)
+            //int count = 0;
+            //for (int i = 1; i <=100; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //Write a program and continuously ask the user to enter a number or "ok" to exit.
+            //Calculate the sum of all the previously entered numbers and display it on the console.
+            bool ok = true;
+            int total = 0;
+            while (ok)
             {
-                if (i % 3 == 0)
+                Console.Write("enter a number or \"ok\" to exit: ");
+                string input = Console.ReadLine();
+                if (input == "ok")
                 {
-                    count++;
+                    ok = !ok;
+                }
+                else
+                {
+                    int add = Int32.Parse(input);
+                    total += add;
+                    Console.WriteLine(total);
+                    continue;
                 }
             }
-            Console.WriteLine(count);
+
         }
     }
 }
