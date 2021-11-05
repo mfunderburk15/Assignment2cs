444aa3e2cc4af58f0c781823a719dd9d1d84905b
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 15:28:24 -0500

    first commit

---
+++ Assignment2cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment2cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
