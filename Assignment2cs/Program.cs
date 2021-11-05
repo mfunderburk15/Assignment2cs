615e71006d8c683276a8de63c33a2b9d675d9812
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 17:27:48 -0500

    question 3 solved

--- Assignment2cs/Program.cs
+++ Assignment2cs/Program.cs
@@ -19,25 +19,38 @@ namespace Assignment2cs
 
             //Write a program and continuously ask the user to enter a number or "ok" to exit.
             //Calculate the sum of all the previously entered numbers and display it on the console.
-            bool ok = true;
-            int total = 0;
-            while (ok)
-            {
-                Console.Write("enter a number or \"ok\" to exit: ");
-                string input = Console.ReadLine();
-                if (input == "ok")
-                {
-                    ok = !ok;
-                }
-                else
-                {
-                    int add = Int32.Parse(input);
-                    total += add;
-                    Console.WriteLine(total);
-                    continue;
-                }
-            }
+            //bool ok = true;
+            //int total = 0;
+            //while (ok)
+            //{
+            //    Console.Write("enter a number or \"ok\" to exit: ");
+            //    string input = Console.ReadLine();
+            //    if (input == "ok")
+            //    {
+            //        ok = true;
+            //    }
+            //    else
+            //    {
+            //        int tadd = Int32.Parse(input);
+            //        total += tadd;
+            //        Console.WriteLine(total);
+            //        continue;
+            //    }
+            //}
 
+            //Write a program and ask the user to enter a number.
+            //Compute the factorial of the number and print it on the console.
+            //For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+
+            Console.Write("Enter a number: ");
+            string answer = Console.ReadLine();
+            int length = Int32.Parse(answer);
+            int total = length;
+            for (int i = length; i > 1; i--)
+            {
+                total *= (i - 1);
+            }
+            Console.WriteLine("{0}! = {1}", length, total );
         }
     }
 }
